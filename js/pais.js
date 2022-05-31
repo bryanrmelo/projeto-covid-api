9dbd1ec5e0a6b4319c22e199295f53a23756e40d
kmil3 <weber.camile@gmail.com>
2022-05-31 17:37:01 -0300

    ccc

---
+++ js/pais.js
@@ -0,0 +1,40 @@
+function handleOnClick() {
+    const pais = document.getElementById("inputPais").value
+    buscaPais(pais)
+}
+
+function consultaWord(palavra) {
+    fetch(`https://api.dicionario-aberto.net/word/${palavra}`, {
+        method : "GET",
+    })
+    .then( (resp) => resp.json() )
+    .then( (data) => {
+        parseWordResponse(data)                    
+    })
+    .catch ( (err) => console.log(err))
+}
+
+function parseWordResponse(data) {
+    var conteudo = '<strong>Definição:</strong><br>'
+        data.map ( (def) => (
+            conteudo += def.xml + '<br><br>'
+        ))
+        document.getElementById("wordResp").innerHTML = conteudo
+}
+
+function buscaPais(pais) {
+    fetch(`https://api.covid19api.com/country/${pais}`, {
+        method : "GET",
+    })
+    .then( (resp) => resp.json() )
+    .then( (data) => {
+        parseNearResponse(data)
+    })
+    .catch ( (err) => console.log(err))
+}
+
+function handleLimparOnClick() {
+    document.getElementById("inputPalavra").value = ''
+    document.getElementById("nearResp").innerHTML = ''
+    document.getElementById("wordResp").innerHTML = ''
+}
