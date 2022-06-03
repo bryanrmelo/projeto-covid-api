db896174c00ff2a2ff7f6f83ce7ce205a5c969fc
Bryan Melo <bryanrmelo@gmail.com>
2022-06-03 17:31:49 -0300

    País

--- js/pais.js
+++ js/pais.js
@@ -1,23 +1,48 @@
-var pais = document.getElementById('input-pais').nodeValue;
-
 function buscarPais() {
-    console.log(pais)
-    fetch(`https://api.covid19api.com/country/${pais}`, {
-        method: "GET",
-    })
-    .then((resp) => resp.json())
-    .then((data) => {
-        console.log(data)
-        //organizarData(data)
-    })
-    .catch((err) => console.log(err))
+    var pais = document.getElementById('pais');
+    fetch(`https://api.covid19api.com/country/${pais.value.toLowerCase()}`, {
+            method: "GET",
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data[data.length - 1])
+            organizarData(data[data.length - 1])
+                //gambiarra eu sei
+        })
+        .catch((err) => console.log(err))
 }
 
 function organizarData(data) {
     var ul = document.getElementById('lista')
 
-    var liNovosConfirmados = document.createElement('li');
-    liNovosConfirmados.appendChild(document.createTextNode('CASOS NOVOS: ' + data.Global.NewConfirmed.toLocaleString()))
-    liNovosConfirmados.className = 'list-group-item';
-    ul.appendChild(liNovosConfirmados);
+    ul.innerHTML = '';
+
+    var liCodigoPais = document.createElement('li');
+    liCodigoPais.appendChild(document.createTextNode('CÓD PAÍS: ' + data.CountryCode))
+    liCodigoPais.className = 'list-group-item';
+    liCodigoPais.style.fontWeight = 'bold';
+    ul.appendChild(liCodigoPais);
+
+    var liAtivos = document.createElement('li');
+    liAtivos.appendChild(document.createTextNode('CASOS ATIVOS: ' + data.Active.toLocaleString()))
+    liAtivos.className = 'list-group-item';
+    ul.appendChild(liAtivos);
+
+    var liConfirmados = document.createElement('li');
+    liConfirmados.appendChild(document.createTextNode('CASOS CONFIRMADOS: ' + data.Confirmed.toLocaleString()))
+    liConfirmados.className = 'list-group-item';
+    liConfirmados.style.fontWeight = 'bold';
+    ul.appendChild(liConfirmados);
+
+    var liCasosRecuperados = document.createElement('li');
+    liCasosRecuperados.appendChild(document.createTextNode('CASOS RECUPERADOS: ' + data.Recovered.toLocaleString()))
+    liCasosRecuperados.className = 'list-group-item';
+    ul.appendChild(liCasosRecuperados);
+
+    var liMortes = document.createElement('li');
+    liMortes.appendChild(document.createTextNode('MORTES CONFIRMADAS: ' + data.Deaths.toLocaleString()))
+    liMortes.className = 'list-group-item';
+    liMortes.style.fontWeight = 'bold';
+    ul.appendChild(liMortes);
+
 }
