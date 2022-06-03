5c9d458ccfd96182b81f885ffc6ed8ffbf2e2274
Bryan Melo <bryanrmelo@gmail.com>
2022-06-03 16:40:44 -0300

    fix?

--- js/pais.js
+++ js/pais.js
@@ -1,54 +1,23 @@
-function handleOnClick() {
-    const pais = document.getElementById("inputPais").value
-    buscaPais(pais)
-    const data = document.getElementById("inputData").value
-    buscaData(datar)
-}
+var pais = document.getElementById('input-pais').nodeValue;
 
-function consultaWord(palavra) {
-    fetch(`https://api.dicionario-aberto.net/word/${palavra}`, {
-        method : "GET",
-    })
-    .then( (resp) => resp.json() )
-    .then( (data) => {
-        parseWordResponse(data)                    
-    })
-    .catch ( (err) => console.log(err))
-}
-
-function parseWordResponse(data) {
-    var conteudo = '<strong>Definição:</strong><br>'
-        data.map ( (def) => (
-            conteudo += def.xml + '<br><br>'
-        ))
-        document.getElementById("wordResp").innerHTML = conteudo
-}
-
-function buscaPais(pais) {
+function buscarPais() {
+    console.log(pais)
     fetch(`https://api.covid19api.com/country/${pais}`, {
-        method : "GET",
+        method: "GET",
     })
-    .then( (resp) => resp.json() )
-    .then( (data) => {
-        parseNearResponse(data)
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data)
+        //organizarData(data)
     })
-    .catch ( (err) => console.log(err))
+    .catch((err) => console.log(err))
 }
 
-function buscaData(datar) {
-    fetch(`https://api.covid19api.com/country/${datar}`, {
-        method : "GET",
-    })
-    .then( (resp) => resp.json() )
-    .then( (data) => {
-        parseNearResponse(data)
-    })
-    .catch ( (err) => console.log(err))
-}
+function organizarData(data) {
+    var ul = document.getElementById('lista')
 
-
-function handleLimparOnClick() {
-    document.getElementById("inputPalavra").value = ''
-    document.getElementById("nearResp").innerHTML = ''
-    document.getElementById("wordResp").innerHTML = ''
+    var liNovosConfirmados = document.createElement('li');
+    liNovosConfirmados.appendChild(document.createTextNode('CASOS NOVOS: ' + data.Global.NewConfirmed.toLocaleString()))
+    liNovosConfirmados.className = 'list-group-item';
+    ul.appendChild(liNovosConfirmados);
 }
